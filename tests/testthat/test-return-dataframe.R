9b50883058eb43a3739eef7a9cd93cbf63f91b6d
Riddle <riddlet@gmail.com>
2019-07-25 16:17:41 -0400

    add an additional test and suppress an expected warning

--- tests/testthat/test-return-dataframe.R
+++ tests/testthat/test-return-dataframe.R
@@ -2,7 +2,7 @@ context('test_return_dataframe.R')
 
 test_that('api call produces dataframe', {
   out1 <- get_metrics('27599104')
-  out2 <- get_metrics('2759910a')
+  out2 <- suppressWarnings(get_metrics('2759910a'))
   expect_is(out1, 'data.frame')
   expect_is(out2, 'data.frame')
 })
@@ -10,5 +10,11 @@ test_that('api call produces dataframe', {
 test_that('data frame is expected shape', {
   out1 <- get_metrics('27599104')
   expect_equal(dim(out1)[1], 1)
-  expect_equal(dim(out1)[2], 13)
+  expect_equal(dim(out1)[2], 24)
+})
+
+test_that('get_metrics can handle multiple ids', {
+  out1 <- get_metrics(c('27599104', '23456789', '23456790'))
+  expect_equal(dim(out1)[1], 3)
+  expect_equal(sum(is.na(out1$relative_citation_ratio)), 0)
 })
